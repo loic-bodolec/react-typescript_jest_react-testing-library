7f8ac137aba5a8423bf53509fdfe935d11e25c24
loicbdev <loicbodolec@msn.com>
2021-11-23 09:30:16 +0100

    finish

--- src/components/Cart.test.tsx
+++ src/components/Cart.test.tsx
@@ -1,24 +1,25 @@
-import { fireEvent, render, screen } from "@testing-library/react"
-import Cart from "./Cart"
+import { fireEvent, render, screen } from "@testing-library/react";
+import Cart from "./Cart";
 
-describe('Cart', ()=>{
-    it('renders a button', ()=>{
-        render(<Cart cancel={()=>{}} />)
-        expect(screen.getByText(/Purchase/i)).toBeInTheDocument()
-    })
+describe("Cart", () => {
+  it("renders a button", () => {
+    render(<Cart cancel={() => {}} />);
+    expect(screen.getByText(/Purchase/i)).toBeInTheDocument();
+  });
 
-    describe('When user clicks purchase button', () => {
-      it('renders Thank you!', () => {
-        render(<Cart cancel={()=>{}} />)
-        fireEvent.click(screen.getByText(/Purchase/i))
-        expect(screen.getByText(/Thank you !/i)).toBeInTheDocument()
-      })
-    })
-    describe('When user clicks cancel button', () => {
-      it('calls cancel function', () => {
-        const cancel = jest.fn()
-        render(<Cart cancel={cancel} />)
-        expect(cancel).toHaveBeenCalledTimes(1)
-      })
-    })
-})
+  describe("When user clicks purchase button", () => {
+    it("renders Thank you!", () => {
+      render(<Cart cancel={() => {}} />);
+      fireEvent.click(screen.getByText(/Purchase/i));
+      expect(screen.getByText(/Thank you !/i)).toBeInTheDocument();
+    });
+  });
+  describe("When user clicks cancel button", () => {
+    it("calls cancel function", () => {
+      const cancel = jest.fn();
+      render(<Cart cancel={cancel} />);
+      fireEvent.click(screen.getByText(/Cancel/i));
+      expect(cancel).toHaveBeenCalledTimes(1);
+    });
+  });
+});
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
-function Cart({cancel}: {cancel: ()=>void}) {
-    const [purchased, setPurchased] = useState(false)
-    return (<div>
-                <button onClick={() => setPurchased(true)}>Purchase</button>
-                <button onClick={cancel}>Cancel</button>
-                { purchased && <p>Thank you !</p>}
-            </div>);
+function Cart({ cancel }: { cancel: () => void }) {
+  const [purchased, setPurchased] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setPurchased(true)}>Purchase</button>
+      <button onClick={cancel}>Cancel</button>
+      {purchased && <p>Thank you !</p>}
+    </div>
+  );
 }
+
 export default Cart;
